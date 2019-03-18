4e5cac1c627648fd8ade50024490d541d0c6bf93
Yuval Adam <_@yuv.al>
2019-03-18 16:54:52 +0200

    Initial ffi calls not working

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
+extern crate bchlib_sys as ffi;
+
+pub fn init(m:i32, t:i32, poly: u32) {
+    unsafe {
+        let bch = ffi::init_bch(m, t, poly);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        init();
     }
 }
